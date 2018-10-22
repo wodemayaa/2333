4c5841b924553d18d8d6005036634b63cf028e0f
wodemayaa <43372898+wodemayaa@users.noreply.github.com>
2018-10-22 15:13:43 +0800

    第一个APP

---
+++ AcitivityTest/app/src/main/java/com/example/a94817/acitivitytest/FirstAcitivity.java
@@ -0,0 +1,59 @@
+package com.example.a94817.acitivitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstAcitivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstAcitivity.this,"Why do you click me ?",
+                        Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(FirstAcitivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+    }
+}
---
+++ AcitivityTest/app/src/main/java/com/example/a94817/acitivitytest/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.a94817.acitivitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
